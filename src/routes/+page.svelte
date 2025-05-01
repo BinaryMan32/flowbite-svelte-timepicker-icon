ea386adcc6968a38a53d2802c510657456951c84
Fred Fettinger <fettinge@gmail.com>
2025-05-01 14:44:15 -0500

    switch to Timepicker with icon example

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,10 @@
 <script>
   import { Label, Timepicker } from 'flowbite-svelte';
+  import { ClockSolid } from 'flowbite-svelte-icons';
 </script>
 
-<Label>Select Time:</Label>
+<Label>Select Time (Flowbite Icon):</Label>
+<Timepicker icon={ClockSolid} />
+
+<Label>Select Time (Default icon):</Label>
 <Timepicker />
